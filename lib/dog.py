1277367c107bcbe097888deee0c873f3e1d21731
Abdullahiomar4 <abdullahiomar58@icloud.com>
2025-11-22 21:48:03 +0300

    Initial commit: add Dog and Person classes with tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+ 
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+
+
+
+    # testing
+
+fido = Dog("Fido")
+snoopy = Dog("Snoopy", "Beagle")
+
+print(fido.name)      
+print(fido.breed)     
+print(snoopy.name)    
+print(snoopy.breed)   
+
+
+fido.bark()           
+
+
+fido.get_adopted("Sophie")
+print(fido.owner)    
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+
+    def __init__(self, name):
+        self.name = name
+
+
+#testing
+
+person1 = Person("Alice")
+print(person1.name)  # Alice
